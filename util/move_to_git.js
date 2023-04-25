ba349a65861456bb675d8f1b749d920730e69320
JJoonS <jjoons@jjoons.com>
2023-04-25 17:04:42 +0900

    Fix some codes
    use strict 철자 수정,
    restUrl 철자 수정,
    URL 이동 주소 수정

--- util/move_to_git.js
+++ util/move_to_git.js
@@ -1,16 +1,16 @@
 function moveToGit() {
-  'usr strict'
+  'use strict'
 
   const userName = 'jjoons'
   const repositoryName = 'kh-beginning-3'
   const branchName = 'main'
 
-  const restURl = location.pathname + location.search + location.hash
+  const restUrl = location.pathname + location.search + location.hash
 
   const originalBaseUrl = `https://${userName}.github.io/${repositoryName}`
   const newBaseUrl = `https://github.com/${userName}/${repositoryName}/blob/${branchName}`
 
   if (location.href.startsWith(originalBaseUrl)) {
-    location.href = `${newBaseUrl}/${restURl.replace(repositoryName, '')}`
+    location.href = `${newBaseUrl}${restUrl.replace('/' + repositoryName, '')}`
   }
 }
