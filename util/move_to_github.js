fdee2a5549b6d01d255de4d271fa4aeac9a5a19c
JJoonS <jjoons@jjoons.com>
2023-04-25 17:13:24 +0900

    Rename to move_to_github

--- util/move_to_github.js
+++ util/move_to_github.js
@@ -1,5 +1,9 @@
-function moveToGit() {
+function moveToGitHub(props) {
   'use strict'
+  props.constructor instanceof Object ||
+    (props = {
+      isFolder: false,
+    })
 
   const userName = 'jjoons'
   const repositoryName = 'kh-beginning-3'
@@ -8,7 +12,9 @@ function moveToGit() {
   const restUrl = location.pathname + location.search + location.hash
 
   const originalBaseUrl = `https://${userName}.github.io/${repositoryName}`
-  const newBaseUrl = `https://github.com/${userName}/${repositoryName}/blob/${branchName}`
+  const newBaseUrl = `https://github.com/${userName}/${repositoryName}/${
+    props.isFolder ? 'tree' : 'blob'
+  }/${branchName}`
 
   if (location.href.startsWith(originalBaseUrl)) {
     location.href = `${newBaseUrl}${restUrl.replace('/' + repositoryName, '')}`
