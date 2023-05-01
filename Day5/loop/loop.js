c4aa6489eed1557ac506aa4c183aae1f3a04adfb
JJoonS <jjoons@jjoons.com>
2023-05-01 11:18:57 +0900

    0501 3

---
+++ Day5/loop/loop.js
@@ -0,0 +1,32 @@
+/*
+파일명: loop.html, loop.js
+for (초기식; 조건식; 증감식) {
+  // 반복할 명령 작성
+}
+*/
+
+// 팝업창에 입력할 수 있는 창이 뜬다
+// prompt('메시지', '입력창에 보여지는 메시지')
+/* let number = prompt('숫자 입력: ', '0')
+alert(number)
+
+let str = prompt('아무 문자나 입력')
+alert(str)
+ */
+
+// 술래잡기
+for (let i = 1; i <= 10; i++) {
+  console.log(i)
+}
+
+console.log('찾는다~')
+
+// 카운트다운
+for (let i = 5; i >= 1; i--) {
+  console.log(i)
+}
+
+console.log('발사!')
+
+// continue
+//   - 반복 흐름을 강제로 다음으로 넘긴다
---
+++ Day5/problem/a2/script.js
@@ -0,0 +1,6 @@
+'use strict'
+
+for (let i = 1; i <= 15; i++) {
+  console.log(i)
+  i % 3 === 0 && i >= 3 && i <= 9 && console.log('짝!')
+}
